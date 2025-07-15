77a7d5fcc6383fa4d121566807dd1f34da3e3e60
N0va-Dr4x <ehijeagbonojoben006@gmail.com>
2025-07-16 00:37:53 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmIH_jzFo16WwMlUs5VUQjGRlp9wMyYzE",
+  authDomain: "lazerpay-8dc69.firebaseapp.com",
+  projectId: "lazerpay-8dc69",
+  storageBucket: "lazerpay-8dc69.firebaseapp.com",
+  messagingSenderId: "188906609529",
+  appId: "1:188906609529:web:34591f318fdf5503c02da8",
+  measurementId: "G-K2T6WZS8PC"
+};
